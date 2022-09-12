925498fdc8fe5c871d11795a412f61db843350c0
CJ <pinedacj1020@gmail.com>
2022-09-13 02:13:54 +0800

    Add function

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let library = [];
+
+
+function Book(name) {
+    this.name = name;
+}
+
+function addBookToLibrary(name) {
+    let book = new Book(name);
+    library.push(book);
+}
+
+function display(){
+    const books = document.querySelector(".books")
+
+    while (books.firstChild){
+        books.removeChild(books.firstChild);
+    }
+
+    library.forEach(library => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        books.appendChild(card);
+        for (let key in library){
+            const para = document.createElement("p");
+            para.textContent = (`${library[key]}`);
+            card.appendChild(para);
+        }
+    })
+}
+
+const btn = document.querySelector("#btn");
+btn.addEventListener('click', ()=>{
+    newbook = document.querySelector("#book");
+    addBookToLibrary(newbook.value);
+    console.log("hello")
+    display();
+})
